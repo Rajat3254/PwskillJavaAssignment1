0b3034f78b6b8d30f1b9c848d3a7ae87192685a7
Rajat Singh <singhr3234@gmail.com>
2023-04-21 23:13:10 +0530

    question 3 is added

---
+++ question_3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class question_3 {
+    public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("The length of rectangle is ");
+    int l=sc.nextInt();
+    System.out.print("The breath of rectangle is ");
+    int b=sc.nextInt();
+    int area=l*b;
+    System.out.print("the area of rectangle is "+area);    
+    }
+    
+}
