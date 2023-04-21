8557dd0b9fd9ba0ad942d045589efdfedd0e1b33
Rajat Singh <singhr3234@gmail.com>
2023-04-21 23:22:06 +0530

    question 4 is added

---
+++ question_4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class question_4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("The cube of ");
+        int num=sc.nextInt();
+        int cube=num*num*num;
+        System.out.print("is found to be "+cube);
+    }
+    
+}
