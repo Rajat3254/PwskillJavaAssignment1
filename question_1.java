8a886c6f960373ecce6cc01154c38f40ae21b371
Rajat Singh <singhr3234@gmail.com>
2023-04-21 21:41:17 +0530

    question 1

---
+++ question_1.java
@@ -0,0 +1,9 @@
+public class question_1 {
+    public static void main(String[] args) {
+        int num_1=2;
+        int num_2=4;
+       int product=num_1*num_2;
+       System.out.print("The product is "+product);
+    }
+    
+}
